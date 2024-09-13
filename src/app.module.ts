bbb930797ec32c5747eeb9d08eeae5ca2a146b34
ValtsuL1 <linnavaltteri@gmail.com>
2024-09-13 06:20:06 +0300

    added postgres config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { env } from 'process';
 
+// database information
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: env.DATABASE_PASSWORD,
+      database: 'fullstackProject',
+      entities: [],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
