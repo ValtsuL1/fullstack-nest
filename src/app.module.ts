0f4562df541d28081aaff815f3c76e9a29517952
ValtsuL1 <linnavaltteri@gmail.com>
2024-09-13 06:33:11 +0300

    created user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { env } from 'process';
+import { UserModule } from './user/user.module';
 
 // database information
 @Module({
@@ -16,7 +17,8 @@ import { env } from 'process';
       database: 'fullstackProject',
       entities: [],
       synchronize: true,
-    })
+    }),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
