32c72bc411f78bc59da954b1bb8b8c7a8b20491f
ValtsuL1 <linnavaltteri@gmail.com>
2024-09-15 13:20:55 +0300

    updated user entity and dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, Matches, MinLength } from "class-validator";
+
+const passwordRegEx =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,20}$/;
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    username: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsNotEmpty()
+    @Matches(passwordRegEx)
+    password: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type: 'varchar', length: '30'})
+    username: string;
+
+    @Column({type: 'varchar', length: '60'})
+    email: string;
+
+    @Column({type: 'varchar', length: '60'})
+    password: string;
+}
